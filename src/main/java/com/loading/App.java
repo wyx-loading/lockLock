5cb99bb8b1127246d2ffe019b64102fb639522cb
wuyuxiang <wuyuxiang@rd.com>
2017-03-06 10:15:14 +0800

    initial commit

---
+++ src/main/java/com/loading/App.java
@@ -0,0 +1,13 @@
+package com.loading;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
